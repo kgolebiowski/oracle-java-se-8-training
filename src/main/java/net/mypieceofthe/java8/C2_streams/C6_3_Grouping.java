1dc0b279b9b56149edd6dac48a9104dbd282f6dc
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-04-30 13:57:02 +0200

    Renamed C6_3_ReducingAndSummarizing to C6_3_Grouping

--- src/main/java/net/mypieceofthe/java8/C2_streams/C6_3_Grouping.java
+++ src/main/java/net/mypieceofthe/java8/C2_streams/C6_3_Grouping.java
@@ -5,7 +5,7 @@ import net.mypieceofthe.java8.domain.dishes.DishDomainUsing;
 /**
  * Created by kgolebiowski on 30/04/2017.
  */
-public class C6_3_ReducingAndSummarizing extends DishDomainUsing {
+public class C6_3_Grouping extends DishDomainUsing {
     public static void main(String[] args) {
 
 
