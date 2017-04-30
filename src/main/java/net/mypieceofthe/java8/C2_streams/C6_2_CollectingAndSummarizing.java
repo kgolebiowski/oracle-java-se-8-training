5d815af503dab47e7241bab69a7df4d6bd854c3f
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-04-30 13:56:14 +0200

    Moved everything from C6_3_ReducingAndSummarizing to C6_2_CollectingAndSummarizing

--- src/main/java/net/mypieceofthe/java8/C2_streams/C6_2_CollectingAndSummarizing.java
+++ src/main/java/net/mypieceofthe/java8/C2_streams/C6_2_CollectingAndSummarizing.java
@@ -52,5 +52,27 @@ public class C6_2_CollectingAndSummarizing extends DishDomainUsing {
                         .map(Dish::getName)
                         .collect(joining(", "))
         );
+
+        // -- Reducing
+
+        assertEquals(
+                getDishList().stream()
+                        .mapToInt(Dish::getCalories).sum(),
+                getDishList().stream()
+                        .collect(reducing(0, Dish::getCalories, (i, j) -> i + j)).intValue()
+        );
+
+        assertEquals(
+                getDishList().stream()
+                        .map(Dish::getCalories).reduce(Integer::sum).get(),
+                getDishList().stream()
+                        .collect(reducing(0, Dish::getCalories, Integer::sum))
+
+        );
+
+        String shortMenu = getDishList().stream().map(Dish::getName)
+                .collect(reducing((s1, s2) -> s1 + s2)).get();
+        String shortMenu2 = getDishList().stream()
+                .collect(reducing("", Dish::getName, (s1, s2) -> s1 + s2));
     }
 }
