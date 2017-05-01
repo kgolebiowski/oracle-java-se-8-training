138afafe982b657b33e8fca3404a29fd37743c05
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-05-01 10:56:06 +0200

    Added streams/iterative processing benchmarks (Chapter 7.1, Part 2)

---
+++ src/main/java/net/mypieceofthe/java8/C2_streams/C7_1_ParallelStreams.java
@@ -0,0 +1,64 @@
+package net.mypieceofthe.java8.C2_streams;
+
+import com.google.common.base.Stopwatch;
+
+import java.util.function.UnaryOperator;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+/**
+ * Created by kgolebiowski on 01/05/2017.
+ */
+public class C7_1_ParallelStreams {
+
+    public static void main(String[] args) {
+        System.out.println("Tests will be executed using " + Runtime.getRuntime().availableProcessors() + " threads\n");
+
+        final long n = 25_000_000;
+
+        System.out.printf("%30s: %s\n", "Iterative",
+                benchmarkExecution(() -> {
+                    long result = 0;
+                    for (long i = 1L; i <= n; i++) {
+                        result += i;
+                    }
+                })
+        );
+
+        UnaryOperator<Long> acc = i -> i + 1;
+
+        System.out.printf("%30s: %s\n", "Sequential infinite stream",
+                benchmarkExecution(() ->
+                        Stream.iterate(1L, acc)
+                                .limit(n)
+                                .reduce(0L, Long::sum))
+        );
+
+        System.out.printf("%30s: %s\n", "Sequential LongStream",
+                benchmarkExecution(() ->
+                        LongStream.rangeClosed(1, n)
+                                .reduce(0L, Long::sum))
+        );
+
+        System.out.printf("%30s: %s\n", "Parallel infinite stream",
+                benchmarkExecution(() ->
+                        Stream.iterate(1L, acc)
+                                .limit(n)
+                                .parallel()
+                                .reduce(0L, Long::sum))
+        );
+
+        System.out.printf("%30s: %s\n", "Parallel LongStream",
+                benchmarkExecution(() ->
+                        LongStream.rangeClosed(1, n)
+                                .parallel()
+                                .reduce(0L, Long::sum))
+        );
+    }
+
+    private static Stopwatch benchmarkExecution(Runnable runnable) {
+        Stopwatch sw = Stopwatch.createStarted();
+        runnable.run();
+        return sw.stop();
+    }
+}
