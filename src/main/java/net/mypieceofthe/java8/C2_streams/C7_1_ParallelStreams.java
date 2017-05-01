524936227e6109db012b1c50965a6ff3031cec36
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-05-01 12:27:36 +0200

    Added more results

--- src/main/java/net/mypieceofthe/java8/C2_streams/C7_1_ParallelStreams.java
+++ src/main/java/net/mypieceofthe/java8/C2_streams/C7_1_ParallelStreams.java
@@ -9,13 +9,36 @@ import java.util.stream.Stream;
 /**
  * Created by kgolebiowski on 01/05/2017.
  *
- * Results on my Macbook Air (1,3 GHz Intel Core i5, JDK 1.8.0_131):
+ * Results on my Macbook Air (1,3 GHz Intel Core i5, JDK 1.8.0_131)
  *
- *                  Iterative: 19.73 ms
+ * For 25_000_000:
+ *
+ *                  Iterative: 19.73 ms <<< 1
  * Sequential infinite stream: 767.7 ms
- *      Sequential LongStream: 32.01 ms
+ *      Sequential LongStream: 32.01 ms < 3
  *   Parallel infinite stream: 10.18 s
- *        Parallel LongStream: 29.35 ms
+ *        Parallel LongStream: 29.35 ms << 2
+ *
+ * For 200_000_000 ("Parallel infinite stream" was disabled as it took to long to execute):
+ *
+ *                  Iterative: 199.9 ms << 2
+ * Sequential infinite stream: 5.532 s
+ *      Sequential LongStream: 212.0 ms < 3
+ *        Parallel LongStream: 116.0 ms <<< 1
+ *
+ *  For 2_000_000_000
+ *  ("Parallel infinite stream" and "Sequential infinite stream" were disabled as they took to long to execute):
+ *
+ *             Iterative: 1.507 s << 2
+ * Sequential LongStream: 1.703 s < 3
+ *   Parallel LongStream: 854.5 ms <<< 1
+ *
+ * For 20_000_000_000L:
+ * ("Parallel infinite stream" and "Sequential infinite stream" were disabled as they took to long to execute):
+ *
+ *             Iterative: 11.04 s << 2
+ * Sequential LongStream: 17.65 s < 3
+ *   Parallel LongStream: 9.158 s <<< 1
  *
  */
 public class C7_1_ParallelStreams {
