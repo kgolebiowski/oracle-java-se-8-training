1e2ccfac05a07249989a4584ebe5e7445a100866
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-05-03 13:48:40 +0200

    Added WordCounterSpliterator fixing parallel stream execution for counting words

--- src/main/java/net/mypieceofthe/java8/C2_streams/C7_3_WordCounter.java
+++ src/main/java/net/mypieceofthe/java8/C2_streams/C7_3_WordCounter.java
@@ -1,35 +1,45 @@
 package net.mypieceofthe.java8.C2_streams;
 
+import java.util.Spliterator;
+import java.util.function.Consumer;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Created by kgolebiowski on 03/05/2017.
  */
 public class C7_3_WordCounter {
 
+    private static final String SAMPLE_TEXT = " Nel   mezzo del cammin  di nostra  vita " +
+            "mi  ritrovai in una  selva oscura" +
+            " ché la  dritta via era   smarrita ";
+
     public static void main(String[] args) {
-        String sampleText =
-                " Nel   mezzo del cammin  di nostra  vita " +
-                        "mi  ritrovai in una  selva oscura" +
-                        " ché la  dritta via era   smarrita ";
 
         C7_3_WordCounter wordCounter = new C7_3_WordCounter();
 
         System.out.println("Words counted iteratively: " +
-                wordCounter.countWordsIteratively(sampleText) + "\n");
+                wordCounter.countWordsIteratively(SAMPLE_TEXT) + "\n");
 
         Stream<Character> sampleSequentialTextStream =
-                IntStream.range(0, sampleText.length()).mapToObj(sampleText::charAt);
+                IntStream.range(0, SAMPLE_TEXT.length()).mapToObj(SAMPLE_TEXT::charAt);
 
         System.out.println("Words counted with Sequential Stream: " +
                 wordCounter.countWordsWithStream(sampleSequentialTextStream) + "\n");
 
         Stream<Character> sampleParallelTextStream =
-                IntStream.range(0, sampleText.length()).mapToObj(sampleText::charAt).parallel();
+                IntStream.range(0, SAMPLE_TEXT.length()).mapToObj(SAMPLE_TEXT::charAt).parallel();
 
         System.out.println("Words counted with Parallel Stream: "+
-                wordCounter.countWordsWithStream(sampleParallelTextStream));
+                wordCounter.countWordsWithStream(sampleParallelTextStream) + "\n");
+
+        Spliterator<Character> spliterator = new WordCounterSpliterator(SAMPLE_TEXT);
+        Stream<Character> sampleParallelTextStreamWithSpliterator =
+                StreamSupport.stream(spliterator, true);
+
+        System.out.println("Words counted with Parallel Stream with custom Spliterator: "+
+                wordCounter.countWordsWithStream(sampleParallelTextStreamWithSpliterator));
     }
 
     private int countWordsIteratively(String input) {
@@ -58,13 +68,13 @@ class WordCounter {
     private final int counter;
     private final boolean lastSpace;
 
-    public WordCounter(int counter, boolean lastSpace) {
+    WordCounter(int counter, boolean lastSpace) {
         this.counter = counter;
         this.lastSpace = lastSpace;
     }
 
-    public WordCounter accumulate(Character c) {
-        System.out.printf("Called accumulate(%s), %s\n", c, this);
+    WordCounter accumulate(Character c) {
+        System.out.printf("WordCounter.accumulate(%s), %s\n", c, this);
         if (Character.isWhitespace(c))
             return lastSpace ? this : new WordCounter(counter, true);
         else
@@ -72,12 +82,12 @@ class WordCounter {
 
     }
 
-    public WordCounter combine(WordCounter wordCounter) {
-        System.out.printf("Called combine(%s) with this %s\n", wordCounter, this);
+    WordCounter combine(WordCounter wordCounter) {
+        System.out.printf("WordCounter.combine(%s) with this %s\n", wordCounter, this);
         return new WordCounter(counter + wordCounter.counter, wordCounter.lastSpace);
     }
 
-    public int getCounter() {
+    int getCounter() {
         return counter;
     }
 
@@ -87,5 +97,50 @@ class WordCounter {
                 "counter=" + counter +
                 ", lastSpace=" + lastSpace +
                 '}';
+    }
+}
+
+class WordCounterSpliterator implements Spliterator<Character> {
+    private final String string;
+    private int currentChar = 0;
+
+    WordCounterSpliterator(String string) {
+        this.string = string;
+    }
+
+    @Override
+    public boolean tryAdvance(Consumer<? super Character> action) {
+        System.out.printf("WordCounterSpliterator.tryAdvance(%s)\n", action);
+        action.accept(string.charAt(currentChar++));
+        return currentChar < string.length();
+    }
+
+    @Override
+    public Spliterator<Character> trySplit() {
+        System.out.printf("WordCounterSpliterator.trySplit()\n");
+        int currentSize = string.length() - currentChar;
+        if (currentSize < 10) {
+            return null;
+        }
+        for (int splitPos = currentSize / 2 + currentChar; splitPos < string.length(); splitPos++) {
+            if (Character.isWhitespace(string.charAt(splitPos))) {
+                Spliterator<Character> spliterator = new WordCounterSpliterator(string.substring(currentChar, splitPos));
+                currentChar = splitPos;
+                return spliterator;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public long estimateSize() {
+        System.out.printf("WordCounterSpliterator.estimateSize()\n");
+        return string.length() - currentChar;
+    }
+
+    @Override
+    public int characteristics() {
+        System.out.printf("WordCounterSpliterator.characteristics()\n");
+        return ORDERED + SIZED + SUBSIZED + NONNULL + IMMUTABLE;
     }
 }
