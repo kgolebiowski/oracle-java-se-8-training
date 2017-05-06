292c3567e0cdcc0b58b34d4389c2b3121f36746c
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-05-06 08:04:24 +0200

    Minor refactoring

--- src/main/java/net/mypieceofthe/java8/C3_EffectiveJava8/C11_CompletableFutures.java
+++ src/main/java/net/mypieceofthe/java8/C3_EffectiveJava8/C11_CompletableFutures.java
@@ -12,17 +12,19 @@ import java.util.stream.Stream;
  * Created by kgolebiowski on 05/05/2017.
  */
 public class C11_CompletableFutures {
-    private Executor executor = Executors.newCachedThreadPool(this::getThreadFactory);
+    private Executor executor = Executors.newCachedThreadPool(this::newThread);
+    //private Executor executor = Executors.newSingleThreadExecutor();
+    //private Executor executor = Executors.newFixedThreadPool(15, this::newThread);
 
     public static void main(String[] args) {
         new C11_CompletableFutures().execute();
     }
 
     private void execute() {
-        System.out.println("Running with " + Runtime.getRuntime().availableProcessors() + " threads");
+        System.out.println("Runtime returns '" + Runtime.getRuntime().availableProcessors() + "' processors/cores");
 
-        List<CompletableFuture<Void>> futures =
-                Stream.generate(SomeComputingClass::new).limit(100)
+        List<CompletableFuture<Void>> futures = Stream.generate(SomeComputingClass::new)
+                .limit(100)
                 .map(computer -> CompletableFuture.supplyAsync(computer::compute, executor))
                 .map(cf -> cf.thenAccept(s -> System.out.println("Finished: " + s)))
                 .collect(Collectors.toList());
@@ -34,7 +36,7 @@ public class C11_CompletableFutures {
         System.out.println("END");
     }
 
-    private Thread getThreadFactory(Runnable r) {
+    private Thread newThread(Runnable r) {
         Thread t = new Thread(r);
         t.setDaemon(true);
         return t;
@@ -46,9 +48,7 @@ class SomeComputingClass {
         long sleepTime = 1000L + new Random().nextInt(1000);
         try {
             Thread.sleep(sleepTime);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        } catch (InterruptedException ignored) { }
         return String.format("My name is '%s' and I was sleeping for '%d'",
                 Thread.currentThread().getName(), sleepTime);
     }
