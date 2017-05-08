3df50a4b53bc711f88a0af0cb4038b192d606907
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-05-08 18:40:46 +0200

    Added "Question 3" from Oracle Samples

---
+++ src/main/java/oraclesamplequestions/Q3.java
@@ -0,0 +1,28 @@
+package oraclesamplequestions;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by kgolebiowski on 08/05/2017.
+ */
+public class Q3 {
+    public static void main(String[] args) {
+        List<String> courses = Arrays.asList("Java", "Oracle", "JSF", "EJB");
+        //int count = courses.stream().filter(s -> s.startsWith("J")).count(); // count returns long !
+        long count = courses.stream().filter(s -> s.startsWith("J")).count();
+        System.out.println(count);
+
+        long assignToLong =  returnsInt(); // int can be implicitly casted to long (no data truncation)
+        //int assignToInt = returnsLong();
+
+    }
+
+    private static int returnsInt() {
+        return 0;
+    }
+
+    private static long returnsLong() {
+        return 0;
+    }
+}
