62ac7ae9e9f2fe4d913a53d838780864a9d228e6
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-05-10 15:18:15 +0200

    Minor refactoring to C6_2_CollectingAndSummarizing

--- src/main/java/net/mypieceofthe/java8/java8inaction/C2_streams/C6_2_CollectingAndSummarizing.java
+++ src/main/java/net/mypieceofthe/java8/java8inaction/C2_streams/C6_2_CollectingAndSummarizing.java
@@ -3,6 +3,8 @@ package net.mypieceofthe.java8.java8inaction.C2_streams;
 import net.mypieceofthe.java8.java8inaction.domain.dishes.Dish;
 import net.mypieceofthe.java8.java8inaction.domain.dishes.DishDomainUsing;
 
+import java.util.stream.Collectors;
+
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.*;
 import static org.junit.Assert.assertEquals;
@@ -13,9 +15,14 @@ import static org.junit.Assert.assertEquals;
 public class C6_2_CollectingAndSummarizing extends DishDomainUsing {
 
     public static void main(String[] args) {
+        new C6_2_CollectingAndSummarizing()
+                .numericCollectors()
+                .stringCollectors()
+                .reducing();
 
-        // -- Numeric collectors
+    }
 
+    private C6_2_CollectingAndSummarizing numericCollectors() {
         assertEquals(
                 getDishList().stream()
                         .count(),
@@ -37,42 +44,47 @@ public class C6_2_CollectingAndSummarizing extends DishDomainUsing {
 
         System.out.println("Calories average: " +
                 getDishList().stream()
-                        .collect(averagingInt(Dish::getCalories))
-        );
+                        .collect(averagingInt(Dish::getCalories)));
 
         // Counts everything in a single operation
         System.out.println(
                 getDishList().stream().collect(summarizingInt(Dish::getCalories))
         );
 
-        // -- String collectors
+        return this;
+    }
 
+    private C6_2_CollectingAndSummarizing stringCollectors() {
         System.out.println(
                 getDishList().stream()
                         .map(Dish::getName)
                         .collect(joining(", "))
         );
 
-        // -- Reducing
+        return this;
+    }
 
+    private C6_2_CollectingAndSummarizing reducing() {
         assertEquals(
                 getDishList().stream()
                         .mapToInt(Dish::getCalories).sum(),
                 getDishList().stream()
-                        .collect(reducing(0, Dish::getCalories, (i, j) -> i + j)).intValue()
+                        .collect(Collectors.reducing(0, Dish::getCalories, (i, j) -> i + j)).intValue()
         );
 
         assertEquals(
                 getDishList().stream()
                         .map(Dish::getCalories).reduce(Integer::sum).get(),
                 getDishList().stream()
-                        .collect(reducing(0, Dish::getCalories, Integer::sum))
+                        .collect(Collectors.reducing(0, Dish::getCalories, Integer::sum))
 
         );
 
         String shortMenu = getDishList().stream().map(Dish::getName)
-                .collect(reducing((s1, s2) -> s1 + s2)).get();
+                .collect(Collectors.reducing((s1, s2) -> s1 + s2)).get();
         String shortMenu2 = getDishList().stream()
-                .collect(reducing("", Dish::getName, (s1, s2) -> s1 + s2));
+                .collect(Collectors.reducing("", Dish::getName, (s1, s2) -> s1 + s2));
+
+        return this;
     }
 }
