3fc5b54095e8fb770adae7d55adb60ca296a18d3
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-05-11 14:13:47 +0200

    Added reduce example using its most rich overloaded version; Refactored

--- src/main/java/net/mypieceofthe/java8/java8inaction/C2_streams/C5_4_Reduce.java
+++ src/main/java/net/mypieceofthe/java8/java8inaction/C2_streams/C5_4_Reduce.java
@@ -3,25 +3,40 @@ package net.mypieceofthe.java8.java8inaction.C2_streams;
 import net.mypieceofthe.java8.java8inaction.domain.dishes.Dish;
 import net.mypieceofthe.java8.java8inaction.domain.dishes.DishDomainUsing;
 
+import java.util.Optional;
+
 /**
  * Created by kgolebiowski on 29/04/2017.
  */
 public class C5_4_Reduce extends DishDomainUsing {
     public static void main(String[] args) {
-        // -- counting
+        new C5_4_Reduce()
+                .summing()
+                .maxAndMin();
+    }
 
-        getDishList().stream()
+    private C5_4_Reduce summing() {
+        Optional<Dish> sumWithoutMapping = getDishList().stream()
                 .reduce((d1, d2) ->
-                        new Dish("", false, d1.getCalories() + d2.getCalories(), Dish.Type.OTHER))
-                .ifPresent(dish -> System.out.println(dish.getCalories()));
+                        new Dish("", false, d1.getCalories() + d2.getCalories(), Dish.Type.OTHER));
 
-        getDishList().stream()
+        sumWithoutMapping.ifPresent(dish -> System.out.println(dish.getCalories()));
+
+        Optional<Integer> sumWithMapReduce = getDishList().stream()
                 .map(Dish::getCalories)
-                .reduce(Integer::sum)
-                .ifPresent(System.out::println);
+                .reduce(Integer::sum);
 
-        // -- max/min
+        sumWithMapReduce.ifPresent(System.out::println);
 
+        Integer sumWithReduceOnly = getDishList().stream()
+                .reduce(0, (sum, dish) -> sum + dish.getCalories(), (s1, s2) -> s1 + s2);
+
+        System.out.println(sumWithReduceOnly);
+
+        return this;
+    }
+
+    private C5_4_Reduce maxAndMin() {
         getDishList().stream()
                 .map(Dish::getCalories)
                 .reduce(Integer::max)
@@ -31,5 +46,7 @@ public class C5_4_Reduce extends DishDomainUsing {
                 .map(Dish::getCalories)
                 .reduce(Integer::min)
                 .ifPresent(c -> System.out.println("Minimum calory dish has " + c));
+
+        return this;
     }
 }
