6048b2915713ea707d2f03f85d13943e7253790c
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-05-12 10:05:08 +0200

    Added review test question for comparing Periods and Durations

--- src/main/java/net/mypieceofthe/java8/java8inaction/C3_EffectiveJava8/C12_1_BasicDateAndTimeApi.java
+++ src/main/java/net/mypieceofthe/java8/java8inaction/C3_EffectiveJava8/C12_1_BasicDateAndTimeApi.java
@@ -4,6 +4,7 @@ import java.time.*;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -87,6 +88,21 @@ public class C12_1_BasicDateAndTimeApi {
                         LocalDate.of(1987, 1, 21)));
 
         assertEquals(Period.ofDays(3), Period.of(0, 0, 3));
+
+        System.out.println();
+
+        // OCP Study Guide: Chapter 5 Dates, Strings, Localization; Review question 15
+        String m1 = Duration.of(1, ChronoUnit.MINUTES).toString();
+        String m2 = Duration.ofMinutes(1).toString();
+        String s = Duration.of(60, ChronoUnit.SECONDS).toString();
+        String d = Duration.ofDays(1).toString();
+        String p = Period.ofDays(1).toString();
+
+        Stream.of(m1, m2, s, d, p)
+                .forEach(s1 -> System.out.println(s1 + " [" + s1.hashCode() + "]"));
+
+        Stream.of(m1 == m2, m1.equals(m2), m1.equals(s), d == p, d.equals(p))
+                .forEach(System.out::println);
 
         return this;
     }
